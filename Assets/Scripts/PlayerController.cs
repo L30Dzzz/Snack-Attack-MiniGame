7235a40dca1721b5f7384b3a0bb2f347c9a15df7
L30Dzzz <113184803+L30Dzzz@users.noreply.github.com>
2023-10-25 12:43:41 -0400

    spawnmanager and destroy

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,20 +4,19 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float verticalInput;
     public float horizontalInput;
     public float speed = 10.0f;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update() 
     {
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+    //fix this tmrw, controlls are inverted
+        verticalInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.forward * verticalInput * Time.deltaTime * speed);
     }
+
 
 }
